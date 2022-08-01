6baa3c40c305694d884573cc8ba4f913ad98bf6a
Havryliuk Roman <60899225+romanesss@users.noreply.github.com>
2022-08-01 13:01:04 +0300

    Add  custom commands

--- cypress/selectors/bank_accounts_page.js
+++ cypress/selectors/bank_accounts_page.js
@@ -10,8 +10,6 @@ export const bank_accounts_page = {
   routing_number_helper_text: '#bankaccount-routingNumber-input-helper-text',
   account_number_helper_text: '#bankaccount-accountNumber-input-helper-text',
   done_button: '[data-test="user-onboarding-next"]',
-  delete_bank_account: '[data-test="bankaccount-list"] li:nth-child(1) [data-test="bankaccount-delete"]',
-  deleted_text_field: '[data-test="bankaccount-list-item-FBJai1FlP"] p',
   log_out_autorized: '[data-test="sidenav-signout"]'
 
 }
